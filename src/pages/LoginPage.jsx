69a81cab3977ee62a8b9e1cb754c89e0b465b86a
Adrian <adrianbueno095@gmail.com>
2024-01-25 09:35:05 +0100

    add a log in and sign up pages

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,5 @@
+const LoginPage = () => {
+    return <h1>Login in</h1>
+}
+
+export default LoginPage;
---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,5 @@
+const SignUpPage = () => {
+    return <h1>Sign Up</h1>;
+}
+
+export default SignUpPage;
