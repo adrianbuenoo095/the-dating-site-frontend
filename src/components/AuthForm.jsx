e99e56c777c9226af374377a95653832c4c97efc
Nadjim Karimi <nadjim73@gmail.com>
2024-01-30 10:57:20 +0100

    add styling to authform

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -56,16 +56,20 @@ const AuthForm = ({ isLogin = false }) => {
       <div className="w-full max-w-xs">
     <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
       <div className="mb-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
           Email
-          <input type="email" required value={email} onChange={handleEmail}/>
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 
+          leading-tight focus:outline-none focus:shadow-outline"
+          id="email" type="email" required value={email} onChange={handleEmail}/>
         </label>
       </div>
       <div className="mb-4">
         <label className="block text-gray-700 text-sm font-bold mb-2">
           Password
-          <input
+          <input className="shadow appearance-none border rounded 
+          w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type="password"
+              placeholder="Password"
               required
               value={password}
               onChange={handlePassword}
@@ -76,46 +80,64 @@ const AuthForm = ({ isLogin = false }) => {
 
       {!isLogin && (
           <>
-            <label className="block text-gray-700 text-sm font-bold mb-2">
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="firstName">
               First Name
-              <input
+              </label>
+              <input  className="shadow appearance-none border rounded w-full 
+              py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   type="text"
+                  id="firstName"
                   required
                   value={firstName}
                   onChange={handleFirstName}
             />
-          </label>
-          <label className="block text-gray-700 text-sm font-bold mb-2">
+             </div>
+          <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="lastName">
             Last Name
-            <input
+          </label>  
+            <input className="shadow appearance-none border rounded w-full 
+            py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type="text"
+              id="lastName"
               required
               value={lastName}
               onChange={handleLastName}
             />
-          </label>
-          <label className="block text-gray-700 text-sm font-bold mb-2">
+            </div>
+            <div className="mb-4">
+<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="age">
             Age
-            <input type="number" required value={age} onChange={handleAge} />
-          </label>
-          <label className="block text-gray-700 text-sm font-bold mb-2">
+            </label>
+            <input className="shadow appearance-none border rounded 
+            w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="number" id="age" required value={age} onChange={handleAge}
+             />
+          </div>
+          <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="status">
             Status
-            <select value={status} onChange={handleStatus} required>
+            </label>
+            <select className="shadow appearance-none border rounded 
+            w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="status"
+            value={status} onChange={handleStatus} required>
               {statusOptions.map((option, index) => (
                 <option key={index} value={option}>
                   {option}
                 </option>
               ))}
             </select>
-          </label>
-        </>
-
-      )}
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">{isLogin ? "Login" : "Signup"}</button>
+          </div>
+          </>
+)}
+      <button className="bg-blue-500 hover:bg-blue-700
+       text-white font-bold py-2 px-4 rounded" type="submit">{isLogin ? "Login" : "Signup"}
+       </button>
     </form>
       </div>
-
-  );
+ );
 };
 
 export default AuthForm;
