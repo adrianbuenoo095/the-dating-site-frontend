4d7abf4c224f61964cd31e1e52daf7ba6303e208
Nadjim Karimi <nadjim73@gmail.com>
2024-01-30 16:31:09 +0100

    changes in the event form

--- src/components/EventForm.jsx
+++ src/components/EventForm.jsx
@@ -68,7 +68,7 @@ const EventForm = () => {
 
      <div className="mb-4">
         <label className="block text-gray-700 text-sm font-bold mb-2">
-          eventDuration
+        EventDuration
           <input className="shadow appearance-none border rounded 
           w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type="Number"
@@ -82,7 +82,7 @@ const EventForm = () => {
 
      <div className="mb-4">
         <label className="block text-gray-700 text-sm font-bold mb-2">
-          location
+          Location
           <input className="shadow appearance-none border rounded 
           w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type="Number"
@@ -96,7 +96,7 @@ const EventForm = () => {
 
      <div className="mb-4">
         <label className="block text-gray-700 text-sm font-bold mb-2">
-          user
+         User
           <input className="shadow appearance-none border rounded 
           w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type="Schema"
