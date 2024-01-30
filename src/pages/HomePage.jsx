37f90db2bae14c8f71f35765e5135ade7c690679
Jesus Cidoncha <jesus.cidoncha.berlanga@gmail.com>
2024-01-30 15:22:58 +0100

    imports

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,9 @@
-<<<<<<< HEAD:src/pages/HomePape.jsx
-import React, { useState, useEffect } from "react";
-import Card from "../components/Card";
-import Navbar from "../components/NavBar";
-=======
 import { useState, useEffect } from "react";
-import Card from "../components/Card"; // Adjust the path as necessary
+import Card from "../components/Card.jsx";
 import Navbar from "../components/Navbar.jsx";
 import Footer from "../components/Footer.jsx";
 import Header from "../components/Header.jsx";
->>>>>>> 2c80d837d6f741c951b585a1b9cc8ddb9679f71f:src/pages/HomePage.jsx
+
 const HomePage = () => {
   const [items, setItems] = useState([]);
 
@@ -39,13 +34,13 @@ const HomePage = () => {
   return (
     <div>
       <Navbar />
-      <Header/>
+      <Header />
       <main className="flex flex-wrap justify-center">
         {items.map((item) => (
           <Card key={item.id} {...item} />
         ))}
       </main>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
