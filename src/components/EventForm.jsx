fc086733821deed88e858f5544e92caf495a4412
Nadjim Karimi <nadjim73@gmail.com>
2024-01-30 17:12:11 +0100

    create button

--- src/components/EventForm.jsx
+++ src/components/EventForm.jsx
@@ -111,7 +111,7 @@ const EventForm = () => {
 
       <button className="bg-blue-500 hover:bg-blue-700
        text-white font-bold py-2 px-4 rounded" type="submit">
-       </button>
+      Create </button>
     </form>
       </div>;
 }
