5ef42b1a4294a9470451b7ce02c6f7bbad64ad14
Adrian <adrianbueno095@gmail.com>
2024-01-30 14:06:00 +0100

    add styling to login and signup

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,6 +1,10 @@
 import AuthForm from "../components/AuthForm";
 const SignUpPage = () => {
-  return <AuthForm />;
+  return (
+      <div className="flex justify-center h-screen items-center mb-6">
+        <AuthForm/>
+      </div>
+  )
 };
 
 export default SignUpPage;
