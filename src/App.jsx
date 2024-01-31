958c668eb5856ca5b6700054128dd7e8f5db5c6b
Nadjim Karimi <nadjim73@gmail.com>
2024-01-31 09:31:20 +0100

    changes in the newevent page and the create event button

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import NotFoundPage from "./pages/NotFoundpage";
 import SignUpPage from "./pages/SignUpPage";
 import NewDogPage from "./pages/NewDogPage";
 import DogDetailsPage from "./pages/DogDetailsPage.jsx";
-
+import NewEventPage from "./pages/NewEventPage";
 function App() {
   return (
     <Routes>
@@ -13,6 +13,7 @@ function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignUpPage />} />
       <Route path="/newdog" element={<NewDogPage />} />
+      <Route path="/newevent" element={<NewEventPage />} />
       <Route path="/dogs/dogId" element={<DogDetailsPage/>} />
       <Route path="*" exact={true} element={<NotFoundPage />} />
     </Routes>
---
+++ src/pages/NewEventPage.jsx
@@ -0,0 +1,9 @@
+import EventForm from "../components/EventForm";
+
+const NewEventPage = () => {
+    return ( 
+
+    <EventForm /> );
+}
+ 
+export default NewEventPage;
