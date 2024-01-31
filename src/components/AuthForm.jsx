d2f744fe54b9c2c019aed0cb7f8463c73372b1e4
Restyled.io <commits@restyled.io>
2024-01-31 21:53:21 +0000

    Restyled by prettier

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,174 +1,176 @@
-import {useContext, useState} from "react";
-import {useNavigate} from "react-router-dom";
-import {AuthContext} from "../context/AuthContext";
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
-const AuthForm = ({isLogin = false}) => {
-    const initialValues = {
-        firstName: "",
-        lastName: "",
-        email: "",
-        passwordHash: "",
-        age: 0,
-        civilStatus: "single",
-        events: [],
-        dog: "",
-    };
+const AuthForm = ({ isLogin = false }) => {
+  const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    passwordHash: "",
+    age: 0,
+    civilStatus: "single",
+    events: [],
+    dog: "",
+  };
 
-    const [data, setData] = useState(initialValues);
+  const [data, setData] = useState(initialValues);
 
-    const handleChange = (e) => {
-        const {name, value} = e.target;
-        setData((prevData) => ({
-            ...prevData,
-            [name]: value,
-        }));
-    };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
-    const statusOptions = [
-        {value: "single", label: "Single"},
-        {value: "married", label: "Married"},
-        {value: "just for fun", label: "Just For Fun"},
-    ];
+  const statusOptions = [
+    { value: "single", label: "Single" },
+    { value: "married", label: "Married" },
+    { value: "just for fun", label: "Just For Fun" },
+  ];
 
-    /* const [errorMsg, setErrorMsg] = useState()
-     */
-    const navigate = useNavigate();
+  /* const [errorMsg, setErrorMsg] = useState()
+   */
+  const navigate = useNavigate();
 
-    const {saveToken} = useContext(AuthContext);
+  const { saveToken } = useContext(AuthContext);
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            const response = await fetch(
-                `${import.meta.env.VITE_API_URL}/auth/${isLogin ? "login" : "signup"}`,
-                {
-                    method: "POST",
-                    headers: {"Content-Type": "application/json"},
-                    body: JSON.stringify(data),
-                },
-            );
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/auth/${isLogin ? "login" : "signup"}`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(data),
+        },
+      );
 
-            if (response.status === 201) {
-                navigate("/login");
-            }
+      if (response.status === 201) {
+        navigate("/login");
+      }
 
-            if (response.status === 200) {
-                const parsed = await response.json();
-                console.log(parsed);
-                saveToken(parsed.token);
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    };
+      if (response.status === 200) {
+        const parsed = await response.json();
+        console.log(parsed);
+        saveToken(parsed.token);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    return (
-        <div className="w-full max-w-xs">
-            <form
-                className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-                onSubmit={handleSubmit}
-            >
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2">
-                        Email
-                        <input
-                            className="shadow appearance-none border rounded
+  return (
+    <div className="w-full max-w-xs">
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={handleSubmit}
+      >
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Email
+            <input
+              className="shadow appearance-none border rounded
           w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            type="email"
-                            name="email"
-                            required
-                            value={data.email}
-                            onChange={handleChange}
-                        />
-                    </label>
-                </div>
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2">
-                        Password
-                        <input
-                            className="shadow appearance-none border rounded
-          w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            type="password"
-                            name="passwordHash"
-                            required
-                            value={data.passwordHash}
-                            onChange={handleChange}
-                        />
-                    </label>
-                </div>
-                {!isLogin && (
-                    <>
-                        <div className="mb-4">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="firstName">
-                                First Name
-                                <input
-                                    className="shadow appearance-none border rounded
-          w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                    type="text"
-                                    name="firstName"
-                                    required
-                                    value={data.firstName}
-                                    onChange={handleChange}
-                                />
-                            </label>
-                        </div>
-                        <div>
-                            <label className="block text-gray-700 text-sm font-bold mb-2">
-                                Last Name
-                                <input
-                                    className="shadow appearance-none border rounded
-          w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                    type="text"
-                                    name="lastName"
-                                    required
-                                    value={data.lastName}
-                                    onChange={handleChange}
-                                />
-                            </label>
-                        </div>
-                        <div>
-                            <label className="block text-gray-700 text-sm font-bold mb-2">
-                                Age
-                                <input
-                                    className="shadow appearance-none border rounded
-          w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                    type="number"
-                                    name="age"
-                                    required
-                                    value={data.age}
-                                    onChange={handleChange}
-                                />
-                            </label>
-                        </div>
-                        <div>
-                            <label className="block text-gray-700 text-sm font-bold mb-2">
-                                Status
-                                <select
-                                    name="civilStatus"
-                                    value={data.civilStatus}
-                                    onChange={handleChange}
-                                    required
-                                >
-                                    {statusOptions.map((option) => (
-
-                                        <option key={option.label} value={option.value}>
-                                            {option.value}
-                                        </option>
-                                    ))}
-                                </select>
-                            </label>
-                        </div>
-                    </>
-                )}
-                <button
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                    type="submit"
-                >
-                    {isLogin ? "Login" : "Signup"}
-                </button>
-            </form>
+              type="email"
+              name="email"
+              required
+              value={data.email}
+              onChange={handleChange}
+            />
+          </label>
         </div>
-    );
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Password
+            <input
+              className="shadow appearance-none border rounded
+          w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="password"
+              name="passwordHash"
+              required
+              value={data.passwordHash}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        {!isLogin && (
+          <>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="firstName"
+              >
+                First Name
+                <input
+                  className="shadow appearance-none border rounded
+          w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="text"
+                  name="firstName"
+                  required
+                  value={data.firstName}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+            <div>
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Last Name
+                <input
+                  className="shadow appearance-none border rounded
+          w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="text"
+                  name="lastName"
+                  required
+                  value={data.lastName}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+            <div>
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Age
+                <input
+                  className="shadow appearance-none border rounded
+          w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="number"
+                  name="age"
+                  required
+                  value={data.age}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+            <div>
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Status
+                <select
+                  name="civilStatus"
+                  value={data.civilStatus}
+                  onChange={handleChange}
+                  required
+                >
+                  {statusOptions.map((option) => (
+                    <option key={option.label} value={option.value}>
+                      {option.value}
+                    </option>
+                  ))}
+                </select>
+              </label>
+            </div>
+          </>
+        )}
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          type="submit"
+        >
+          {isLogin ? "Login" : "Signup"}
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default AuthForm;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,67 +1,69 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import Card from "../components/Card.jsx";
 import Navbar from "../components/Navbar.jsx";
 import Footer from "../components/Footer.jsx";
 import Header from "../components/Header.jsx";
 
 const HomePage = () => {
-        const [dogs, setDogs] = useState([]);
-        const [users, setUsers] = useState([]);
-        const fetchDogs = async () => {
-            try {
-                const dogResponse = await fetch(`${import.meta.env.VITE_API_URL}/api/dogs`);
-                if (dogResponse.ok) {
-                    const dogsData = await dogResponse.json();
-                    console.log(dogs);
-                    setDogs(dogsData)
-                }
-            } catch (error) {
-                console.log(error);
-            }
-        }
-
-        const fetchUsers = async () => {
-            try {
-                const userResponse = await fetch(`${import.meta.env.VITE_API_URL}/api/users`);
-                if (userResponse.ok) {
-                    const usersData = await userResponse.json();
-                    console.log(usersData)
-                    setUsers(usersData)
-                }
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        }
-
-        useEffect(() => {
-            fetchDogs()
-        }, []);
-
-        useEffect(() => {
-            fetchUsers()
-        }, []);
-
-        let items = combineData(dogs, users)
-        function combineData(dogs, users) {
-            return dogs.map((dog) => {
-                const owner = users.find((user) => user.id === dog.userId);
-                return {...dog, owner};
-            });
-        }
-
-        return (
-            <div>
-                <Navbar/>
-                <Header/>
-                <main className="flex flex-wrap justify-center">
-                    {items.map((item) => (
-                        <Card key={item.id} {...item} />
-                    ))}
-                </main>
-                <Footer/>
-            </div>
-        );
+  const [dogs, setDogs] = useState([]);
+  const [users, setUsers] = useState([]);
+  const fetchDogs = async () => {
+    try {
+      const dogResponse = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/dogs`,
+      );
+      if (dogResponse.ok) {
+        const dogsData = await dogResponse.json();
+        console.log(dogs);
+        setDogs(dogsData);
+      }
+    } catch (error) {
+      console.log(error);
     }
-;
+  };
 
+  const fetchUsers = async () => {
+    try {
+      const userResponse = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/users`,
+      );
+      if (userResponse.ok) {
+        const usersData = await userResponse.json();
+        console.log(usersData);
+        setUsers(usersData);
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchDogs();
+  }, []);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  let items = combineData(dogs, users);
+  function combineData(dogs, users) {
+    return dogs.map((dog) => {
+      const owner = users.find((user) => user.id === dog.userId);
+      return { ...dog, owner };
+    });
+  }
+
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <main className="flex flex-wrap justify-center">
+        {items.map((item) => (
+          <Card key={item.id} {...item} />
+        ))}
+      </main>
+      <Footer />
+    </div>
+  );
+};
 export default HomePage;
