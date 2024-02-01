1612979297c74c204515a09e21cc627f8c6e5c04
Adrian <adrianbueno095@gmail.com>
2024-02-01 14:06:08 +0100

    add styling to dogForm

--- src/components/DogForm.jsx
+++ src/components/DogForm.jsx
@@ -45,7 +45,7 @@ const DogForm = () => {
     };
 
     return (
-        <div className="w-full max-w-xs">
+        <div className="flex justify-center h-screen items-center mb-6">
             <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
