b082d18036e9ab6301bc74d9e2c03679fc778834
Adrian <adrianbueno095@gmail.com>
2024-02-06 16:27:57 +0100

    refactor eventCard

--- src/components/Event/EventCard.jsx
+++ src/components/Event/EventCard.jsx
@@ -1,38 +1,30 @@
-
 const EventCard = ({event}) => {
     return (
-        <div>
-                <div className="max-w-screen-lg mx-auto p-6 bg-white rounded-md shadow-md flex">
-                            <div className="w-1/2 mr-6">
-                                <img
-                                    src={event.picture}
-                                    alt={event.name}
-                                    className="w-full h-auto rounded-md"
-                                />
-                            </div>
-                            <div className="w-1/2">
-                                <div className="mb-4">
-                                    <p>
-                                        <span className="font-semibold">Event Name:</span>{" "}
-                                        {event.name}
-                                    </p>
-                                    <p>
-                                        <span className="font-semibold">Time:</span> {event.time}
-                                    </p>
-                                    <p>
-                                        <span className="font-semibold">Duration:</span>{" "}
-                                        {event.eventDuration}
-                                    </p>
-                                    <p>
-                                        <span className="font-semibold">Location:</span>{" "}
-                                        {event.location}
-                                    </p>
-                                </div>
-                                <p className="mb-4">
-                                    <span className="font-semibold">Owner:</span> {event.user}
-                                </p>
-                            </div>
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <img
+                    src={event.picture}
+                    alt={event.name}
+                    className="rounded-t-lg"
+                />
+            <div className="p-5">
+                    <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        {event.name}
+                    </h2>
+                    <p>
+                        <span className="font-semibold">Time:</span> {event.time}
+                    </p>
+                    <p>
+                        <span className="font-semibold">Duration:</span>{" "}
+                        {event.eventDuration}
+                    </p>
+                    <p>
+                        <span className="font-semibold">Location:</span>{" "}
+                        {event.location}
+                    </p>
                 </div>
+                <p className="mb-4">
+                    <span className="font-semibold">Owner:</span> {event.user}
+                </p>
         </div>
     );
 };
