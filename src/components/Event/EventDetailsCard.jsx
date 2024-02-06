5485091ce255158c0e5227e65f208d245e3dd149
Adrian <adrianbueno095@gmail.com>
2024-02-06 16:47:29 +0100

    refactor code and add new EventDetailsCard component

--- src/components/Event/EventDetailsCard.jsx
+++ src/components/Event/EventDetailsCard.jsx
@@ -1,6 +1,5 @@
 const EventDetailsCard = ({event}) => {
     return (
-        <div>
             <div className="min-h-screen flex items-center justify-center bg-gray-100">
                 <div className="max-w-screen-lg mx-auto p-6 bg-white rounded-md shadow-md flex">
                     <div className="w-1/2 mr-6">
@@ -8,7 +7,6 @@ const EventDetailsCard = ({event}) => {
                     </div>
                 </div>
             </div>
-        </div>
     );
 };
 
