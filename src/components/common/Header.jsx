70abe01b1f9366ff9dc8e93f58f7a1211a933b17
Adrian <adrianbueno095@gmail.com>
2024-02-09 22:40:05 +0100

    update header, vite.config

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,4 +1,4 @@
-import headerImage from "/public/assets/images/puppy.jpg";
+import headerImage from "/assets/images/puppy.jpg";
 import {Button} from "./Button.jsx";
 const Header = () => {
     return (
--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   root:'./',
-  build:{
-    outDir:'dist',
+  build: {
+    outDir: 'public',
   },
-  publicDir:'assets',
   plugins: [react()],
 })
