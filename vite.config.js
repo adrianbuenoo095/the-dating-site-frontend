61eefd7c39f066b4c040bdbf215c01096b4ddd28
Adrian <adrianbueno095@gmail.com>
2024-02-13 22:03:54 +0100

    fix issue

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
-
+import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
         outDir: 'public',
     },
+    esbuildOptions: {
+        jsx: "automatic",
+    },
+    plugins: [react()],
 })
