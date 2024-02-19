fc15d8f02672498b7487ecf9d2b6249556ea6357
Adrian <adrianbueno095@gmail.com>
2024-02-19 11:28:34 +0100

    update test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,16 @@
 describe('My First Test', () => {
-  it('Does not do much!!', () => {
-    expect(true).to.equal(true)
+  it('clicks the link "type"', () => {
+    cy.visit('https://example.cypress.io')
+    cy.contains('type').click()
+
+    cy.url().should('include', '/commands/actions')
+
+    cy.get('.action-email')
+        .type('fake@email.com')
+        .should('have.value','fake@email.com')
+
+
+
+
   })
 })
