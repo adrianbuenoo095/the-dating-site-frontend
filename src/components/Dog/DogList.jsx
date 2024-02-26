946bd4b97735696332a2b463dd0f0e041f64d785
Adrian <adrianbueno095@gmail.com>
2024-02-26 10:41:57 +0100

    refactor code

--- src/components/Dog/DogList.jsx
+++ src/components/Dog/DogList.jsx
@@ -1,48 +1,26 @@
-import {useEffect, useState} from "react";
 import DogCard from "./DogCard.jsx";
+import {useFetchData} from "../../hooks /useFetchData.js";
 
 const DogList = () => {
-        const [dogs, setDogs] = useState([]);
-        const [users, setUsers] = useState([]);
-        const fetchDogs = async () => {
-            try {
-                const dogResponse = await fetch(`${import.meta.env.VITE_API_URL}/api/dogs`);
-                if (dogResponse.ok) {
-                    const dogsData = await dogResponse.json();
-                    setDogs(dogsData)
-                }
-            } catch (error) {
-                console.log(error);
-            }
-        }
+        const pathRoute = "/api/dogs";
+        const usePathRoute = "/api/users";
+        const dogData = useFetchData(`${import.meta.env.VITE_API_URL}${pathRoute}`)
+        const userData=  useFetchData(`${import.meta.env.VITE_API_URL}${usePathRoute}`)
 
-        const fetchUsers = async () => {
-            try {
-                const userResponse = await fetch(`${import.meta.env.VITE_API_URL}/api/users`);
-                if (userResponse.ok) {
-                    const usersData = await userResponse.json();
-                    setUsers(usersData)
-                }
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        }
+        console.log("dogData", dogData)
+        console.log("userData", userData)
 
-        useEffect(() => {
-            fetchDogs()
-        }, []);
-
-        useEffect(() => {
-            fetchUsers()
-        }, []);
-
-        let userDogs = combineData(dogs, users)
+        let userDogs = combineData(dogData, userData)
 
         function combineData(dogs, users) {
-            return dogs.map((dog) => {
-                const owner = users.find((user) => user.id === dog.userId);
+            if (!dogs || !users) return;
+
+            const dogOwner = dogs.map((dog) => {
+                let owner = users.find((user) => user.id === dog.userId);
                 return {...dog, owner};
             });
+
+            return dogOwner;
         }
 
         return (
